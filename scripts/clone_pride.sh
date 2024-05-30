226b99754dc66035f84ec7e75f9ba5a0e0c1448e
Ana Huaman <ana@traclabs.com>
2024-05-30 15:55:16 -0500

    Updated branch for pride

--- scripts/clone_pride.sh
+++ scripts/clone_pride.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PRIDE_BRANCH='develop' # develop, maars-devel
-SPACEROS_PROCEDURE_SVN='https://svn.traclabs.com/svn/pride/trunk/procedures/spaceros_demos'
+PRIDE_BRANCH='demos/brash' # develop, maars-devel
+SPACEROS_PROCEDURE_SVN='https://svn.traclabs.com/svn/pride/trunk/procedures/BRASH'
 PRIDE_SYSREP_SVN='https://svn.traclabs.com/svn/ontology/trunk/docs/yaml_sysreps'
    
 git clone git@bitbucket.org:traclabs/pride.git -b $PRIDE_BRANCH
